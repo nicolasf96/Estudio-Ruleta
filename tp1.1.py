5190791f8ec8806aae8340c547fb5e2a8d0fd693
Nicolás Fierro <nico.fierro1@gmail.com>
2021-03-24 11:25:53 -0300

    Commit Inicial

---
+++ tp1.1.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#TP 1.1 Simulación de Una Ruleta
+#Datos
+
+frecuencia: 1/37
+esperanza= np.arange(0, 37).mean()
+desvio= np.arange(0, 37).std()
+varianza= np.arange(0, 37).var()
